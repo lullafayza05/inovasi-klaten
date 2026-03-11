39ed419089301ed7bd9dc20d20e7be16aad7295e
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-11 09:37:50 +0700

    modif app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
-import Inovasi from "./pages/Inovasi";
-import "./App.css";
-
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Header />
-
+    <>
       <Navbar />
-
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/inovasi" element={<Inovasi />} />
-        </Routes>
-      </div>
-
-    </BrowserRouter>
+      <Dashboard />
+    </>
   );
 }
 
