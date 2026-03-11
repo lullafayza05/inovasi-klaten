7e8f2d57cc1bc5869cc7fc09c5bcb59e76c827a4
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-11 09:38:18 +0700

    modif header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 function Header() {
   return (
-    <div className="header">
-      <div className="logo">
-        <img src="/logo.png" alt="logo" />
-      </div>
+    <div style={{
+      background: "#0c3b63",
+      color: "white",
+      padding: "40px",
+      borderRadius: "10px",
+      display: "flex",
+      alignItems: "center",
+      gap: "30px"
+    }}>
+      <img
+        src="/hai.png"
+        alt="hai"
+        style={{ width: "140px", borderRadius: "10px" }}
+      />
 
-      <div className="title">
-        <h2>Sistem Inovasi Daerah</h2>
-        <p>Pemerintah Kabupaten Klaten</p>
+      <div>
+        <h2>
+          Inovasi penting dalam kehidupan yang berkembang dan dinamis sehingga
+          perlu berkreasi dan berinovasi di semua bidang termasuk pemerintahan.
+        </h2>
+
+        <p style={{ marginTop: "10px" }}>
+          <b>bapak</b> <br />
+          Menteri Dalam Negeri
+        </p>
       </div>
     </div>
   );
