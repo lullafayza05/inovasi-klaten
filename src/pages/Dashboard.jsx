ed15714fdc326486e42b762610d87763837e3601
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-11 08:14:41 +0700

    dashboard..jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,26 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <div className="card-container">
+        <div className="card">
+          <h3>Total Inovasi</h3>
+          <h2>25</h2>
+        </div>
+
+        <div className="card">
+          <h3>Total OPD</h3>
+          <h2>12</h2>
+        </div>
+
+        <div className="card">
+          <h3>Total Inovator</h3>
+          <h2>40</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
