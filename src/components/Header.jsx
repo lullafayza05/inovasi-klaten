db9540336dadfa5b11e912c327d4afeacb4ab722
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-11 08:14:09 +0700

    header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+      </div>
+
+      <div className="title">
+        <h2>Sistem Inovasi Daerah</h2>
+        <p>Pemerintah Kabupaten Klaten</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
