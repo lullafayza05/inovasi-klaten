f397b02cea8ef3602377af2db5c62b3f4b9ae862
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-11 08:14:50 +0700

    inovasi.jsx

---
+++ src/pages/Inovasi.jsx
@@ -0,0 +1,33 @@
+function Inovasi() {
+  return (
+    <div className="content">
+
+      <h1>Data Inovasi</h1>
+
+      <div className="card">
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Inovasi</th>
+              <th>OPD</th>
+              <th>Tahun</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Aplikasi Pelayanan Desa</td>
+              <td>Diskominfo</td>
+              <td>2024</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  );
+}
+
+export default Inovasi;
