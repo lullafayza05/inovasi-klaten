a40a3815826ac7bebcade4befd5a4eac350b742c
l200230152@student.ums.ac.id <l200230152@student.ums.ac.id>
2026-03-30 19:41:11 +0700

    update views backend

--- backend/inovasi/views.py
+++ backend/inovasi/views.py
@@ -1,8 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Inovasi
+from .serializers import InovasiSerializer
 
 @api_view(['GET'])
 def get_inovasi(request):
-    data = Inovasi.objects.all().values()
-    return Response(list(data))
+    data = Inovasi.objects.all()
+    serializer = InovasiSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_inovasi(request):
+    serializer = InovasiSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
